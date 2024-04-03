7c6228839f1a2eb9c6610cc1677625950c84b054
giannejunior <79637830+GabrieleGiannessi@users.noreply.github.com>
2024-04-03 15:09:40 +0200

    Aggiunta la gui

---
+++ interfaccia2024/provapl.sql
@@ -0,0 +1,57 @@
+create or replace procedure provapl IS
+BEGIN
+    gui.APRIPAGINA('Registrazione');
+    gui.AGGIUNGIFORM;  
+
+        gui.AGGIUNGIRIGAFORM;   
+            gui.aggiungiIntestazione(testo => 'Registrazione', dimensione => 'h2');
+            gui.AGGIUNGIGRUPPOINPUT; 
+                gui.AGGIUNGICAMPOFORM (classeIcona => 'fa fa-user', nome => 'Nome', placeholder => 'Nome');        
+                gui.AGGIUNGICAMPOFORM (classeIcona => 'fa fa-user', nome => 'Cognome', placeholder => 'Cognome');        
+                gui.AGGIUNGICAMPOFORM (tipo => 'email', classeIcona => 'fa fa-envelope', nome => 'Email', placeholder => 'Indirizzo Email');   
+                gui.AGGIUNGICAMPOFORM (tipo => 'password', classeIcona => 'fa fa-key', nome => 'Password', placeholder => 'Password'); 
+                gui.AGGIUNGICAMPOFORM (tipo => 'password', classeIcona => 'fa fa-phone', nome => 'Telefono', placeholder => 'Telefono'); 
+            gui.CHIUDIGRUPPOINPUT;
+        gui.CHIUDIRIGAFORM; 
+
+        gui.AGGIUNGIRIGAFORM; 
+
+           gui.APRIDIV (classe => 'col-half');
+           gui.aggiungiIntestazione(testo => 'Data di nascita', dimensione => 'h4'); 
+
+                gui.AGGIUNGIGRUPPOINPUT; 
+                    gui.APRIDIV (classe => 'col-third');
+                        gui.AGGIUNGIINPUT (placeholder => 'DD', nome => 'Day', classe => ''); 
+                    gui.CHIUDIDIV;
+
+                    gui.APRIDIV (classe => 'col-third');
+                        gui.AGGIUNGIINPUT (placeholder => 'MM', nome => 'Month', classe => ''); 
+                    gui.CHIUDIDIV;
+
+                    gui.APRIDIV (classe => 'col-third');
+                        gui.AGGIUNGIINPUT (placeholder => 'YYYY', nome => 'Year', classe => ''); 
+                    gui.CHIUDIDIV;
+
+                gui.CHIUDIGRUPPOINPUT; 
+            gui.CHIUDIDIV;
+            gui.APRIDIV (classe => 'col-half'); 
+                gui.aggiungiIntestazione(testo => 'Sesso', dimensione => 'h4');
+
+                    gui.AGGIUNGIGRUPPOINPUT; 
+                        gui.AGGIUNGIINPUT (nome => 'gender', classe => '', ident => 'gender-male', tipo => 'radio');
+                        gui.AGGIUNGILABEL (target => 'gender-male', testo => 'Maschio');  
+                        gui.AGGIUNGIINPUT (nome => 'gender', classe => '', ident => 'gender-female',    tipo => 'radio');
+                        gui.AGGIUNGILABEL (target => 'gender-female', testo => 'Femmina'); 
+                    gui.CHIUDIGRUPPOINPUT;  
+            gui.CHIUDIDIV;
+        gui.CHIUDIRIGAFORM; 
+
+        gui.AGGIUNGIRIGAFORM;
+            gui.AGGIUNGIGRUPPOINPUT; 
+                gui.AGGIUNGIBOTTONESUBMIT (nome => 'Registra', value => 'Registra'); 
+            gui.CHIUDIGRUPPOINPUT; 
+        gui.CHIUDIRIGAFORM; 
+
+    gui.CHIUDIFORM; 
+
+    END provapl; 
